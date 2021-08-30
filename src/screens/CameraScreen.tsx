f2efcfedeff96e1178b5939516e31b00539a6867
Kevin Saucedo <user002_@outlook.com>
2021-08-30 15:11:32 -0500

    Acceso a camara y lectura de archivo

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,12 +1,69 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { TextInput } from 'react-native-gesture-handler'
+import React, { useRef } from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+// import { Dirs, FileSystem } from 'react-native-file-access'
 
 export const CameraScreen = () => {
+    const cameraRef = useRef<any>()
+    const takePicture = async () => {
+        if (cameraRef) {
+            const options = { quality: 0.5, base64: true };
+            const data = await cameraRef.current.takePictureAsync(options);
+            console.log(data.uri);
+
+        }
+    };
     return (
-        <View>
-            <Text>Hola bb!! camera</Text>
-            <TextInput />
+        <View style={styles.container}>
+            <RNCamera
+                ref={cameraRef}
+                style={styles.preview}
+                type={RNCamera.Constants.Type.back}
+                flashMode={RNCamera.Constants.FlashMode.on}
+                androidCameraPermissionOptions={{
+                    title: 'Permission to use camera',
+                    message: 'We need your permission to use your camera',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancel',
+                }}
+                androidRecordAudioPermissionOptions={{
+                    title: 'Permission to use audio recording',
+                    message: 'We need your permission to use your audio',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancel',
+                }}
+            />
+            <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                <TouchableOpacity onPress={takePicture} style={styles.capture}>
+                    <Text style={{ fontSize: 14 }}> SNAP </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black',
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
+    },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
-import { View, Text} from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { HeaderBarExitButton } from '../components/HeaderBarExitButton';
+import { Dirs, FileSystem } from 'react-native-file-access'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const HomeScreen = () => {
-    return (        
-       <View>
-           <HeaderBarExitButton title="Home"/>
-           <Text>Hola bb!! home</Text>
-       </View>
+    const [medias, setMedia] = useState<string[]>([])
+    FileSystem.ls(`${Dirs.CacheDir}/Camera`).then((files) => {
+        let fs: string[] = []
+        files.forEach((fName) => {
+            let path = `file://${Dirs.CacheDir}/Camera/${fName}`
+            fs = [...fs, path]
+        })
+        setMedia(fs)
+    })
+
+    return (
+        <View>
+            <HeaderBarExitButton title="Home" />
+            <Text>Hola bb!! home</Text>
+            {medias.map((f) => (
+                <Image
+                    key={f}
+                    style={{ width: 100, height: 100 }}
+                    source={{
+                        uri: f
+                    }}
+                />
+            ))
+            }
+
+        </View>
     )
 }
 
