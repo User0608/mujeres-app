b0e5a0200f90821bfc6c17d57ac35b8378bda6a0
Kevin Saucedo <user002_@outlook.com>
2021-08-30 02:23:36 -0500

    Vista de login

--- src/components/CButton.tsx
+++ src/components/CButton.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
-import { View, Text, StyleSheet} from 'react-native'
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    StyleProp,
+    ViewStyle
+} from 'react-native'
 
-export const CButton = () => {
+interface Props {
+    onPress: (id?: string) => void
+    id?: string
+    text: string
+    style?: StyleProp<ViewStyle>
+    color?: string
+}
+
+export const CButton = ({color, text, id, style, onPress }: Props) => {
     return (
-        <div>
-            
-        </div>
+        <View style={[styles.bordes, style]}>
+            <TouchableOpacity 
+            onPress={()=>onPress(id)}
+            style={{ ...styles.small}}
+            >
+                <Text style={{ color: color,fontSize:15 }}>
+                    {text}
+                </Text>
+            </TouchableOpacity>
+        </View >
     )
 }
+const styles = StyleSheet.create({
+    bordes: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 20,
+    },
+    small: {        
+        height: '100%',
+        width:'110%',
+        justifyContent:'center',
+        alignItems:'center',
+    }
+});
--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,9 +1,10 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack'
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, Button } from 'react-native';
 import { CInput } from '../components/CInput';
 import { RootStackParamList } from '../helpers/RootStackParams';
+import { CButton } from '../components/CButton';
 
 type authScreenProp = StackNavigationProp<RootStackParamList, 'SignIn'>
 
@@ -13,9 +14,16 @@ export const SignInScreen = () => {
     const handlerChange = (name: string, value: string) => {
         console.log(name, ":", value)
     }
+    const handlerLogin = () => {
+
+    }    
     return (
         <View style={styles.container}>
             <View style={styles.loginBox}>
+                <Image
+                    style={styles.imagen}
+                    source={require('../../assets/logo.png')}
+                />
                 <CInput
                     name="username"
                     label="Username"
@@ -27,22 +35,59 @@ export const SignInScreen = () => {
                     label="Password"
                     onChange={handlerChange}
                 />
+
+                <CButton
+                    style={styles.loginButton}
+                    text="Iniciar"
+                    onPress={handlerLogin}
+                />
+
             </View>
+            <CButton
+                style={styles.bottomRight}
+                text="Registrarse"
+                onPress={() => { navigation.navigate('SignUp') }}
+            />
+
         </View>
     )
 }
-{/* <Text>Hola bb!!</Text>
-  <Button title="ok" onPress={() => { navigation.navigate('SignUp') }} /> */}
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    loginBox:{
-        padding:20,
-        width:440,
-        height:300,
-        borderRadius:20,
-        borderWidth:1
+    loginBox: {
+        justifyContent: 'center',
+        padding: 20,
+        width: 440,
+        height: 450,
+        borderRadius: 20,
+        borderWidth: 1,
+        bottom: 100,
+    },
+    imagen: {
+        alignSelf: 'center',
+        width: 150,
+        height: 180,
+        marginBottom: 30,
+    },
+    bottomRight: {
+        position: 'absolute',
+        bottom: 40,
+        right: 40,
+        backgroundColor: '#00c853',
+    },
+    loginButton: {
+        position: 'absolute',
+        backgroundColor: "#ffb04c",
+        bottom: -30,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 300,
+        alignSelf: 'center'
     }
 });
